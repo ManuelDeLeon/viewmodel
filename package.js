f91c464f11ba36ae155a6add4a2a8e2cf6af9ac0
Manuel De Leon <manueldeleon@gmail.com>
2014-11-21 16:51:40 -0700

    Commit all so far

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
     api.use('coffeescript', ['client', 'server']);
-    api.use('manuel:reactivearray', 'client')
+    api.use('manuel:reactivearray@1.0.0', 'client')
     api.addFiles(['viewmodel.coffee'], 'client');
     api.export('ViewModel');
 });
