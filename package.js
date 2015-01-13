1e85d60f76151b8c36f1736e188ac7345e62709b
Manuel De Leon <manueldeleon@gmail.com>
2015-01-13 11:24:43 -0700

    Commit all so far

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "MVVM framework for Meteor",
-  version: "1.2.3",
+  summary: "MVVM framework for Meteor. (Knockout for Meteor)",
+  version: "1.2.4",
   git: "https://github.com/ManuelDeLeon/viewmodel"
 });
 
