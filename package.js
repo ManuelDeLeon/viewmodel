908ab91891c1443292f5198f190e1e875edb3631
Manuel De Leon <manueldeleon@gmail.com>
2015-04-09 10:43:43 -0600

    Update ReactiveArray version

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0.4');
     api.use('coffeescript');
     api.use('blaze', 'client');
-    api.use('manuel:reactivearray@1.0.3');
+    api.use('manuel:reactivearray@1.0.5');
     api.addFiles('viewmodel.coffee', 'client');
     api.addFiles('hook.coffee', 'client');
     api.export('ViewModel', 'client');
