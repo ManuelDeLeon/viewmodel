76c376a7255886024c24ab0f20a2bbff602c9138
unknown <manuel.de.leon@MW7CDTPCX98PZD.dir.svc.accenture.com>
2015-11-19 10:54:05 -0700

    Update version

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
-﻿## 2.0.0
+﻿## 2.1.0
+
+* Add persist option for individual view models
+
+## 2.0.0
 
 * Hello World!
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'manuel:viewmodel',
   summary: "MVVM, two-way data binding, and components for Meteor. Similar to Angular and Knockout.",
-  version: "2.0.0",
+  version: "2.1.0",
   git: "https://github.com/ManuelDeLeon/viewmodel"
 });
 
